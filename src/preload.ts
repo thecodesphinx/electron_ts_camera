ef810c2f33b56f021ad4dcb8b459a4e38e95047e
Janavi Anand <janavi@Admins-MBP.home>
2020-11-04 18:48:58 -0500

    adds video element

--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,28 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded",async () => {
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
     if (element) {
       element.innerText = text;
     }
+
   };
 
   for (const type of ["chrome", "node", "electron"]) {
     replaceText(`${type}-version`, (process.versions as any)[type]);
   }
+
+  const live_video = document.getElementById(
+    "local_video"
+  ) as HTMLVideoElement;
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+  });
+
+  //Set video source object
+  live_video.srcObject = stream;
+  live_video.play();
+
 });
